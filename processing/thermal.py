c56e7d3f9b33a3240faca0e4b0ff1267808cdcf6
gferraro <g.ferraro22@gmail.com>
2020-08-25 11:39:09 +1200

    add predictions into database

--- processing/thermal.py
+++ processing/thermal.py
@@ -199,6 +199,8 @@ def upload_tracks(api, recording, main_model, model_results, logger):
 def add_track_tags(api, recording, track, model, logger):
     track_data = {"name": model["name"], "algorithmId": model["algiorithm_id"]}
     track_data["all_class_confidences"] = track.get("all_class_confidences")
+    track_data["predictions"] = track.get("predictions")
+
     if track and "tag" in track:
         logger.debug("adding %s track tag for track %s", model["name"], track["id"])
         api.add_track_tag(recording, track, data=track_data)
