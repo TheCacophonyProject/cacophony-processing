2f88589fd7882fb78d2019dc6915d1a843c86c93
gferraro <g.ferraro22@gmail.com>
2023-12-13 14:43:31 +1300

    add mimetype

--- processing/audio_analysis.py
+++ processing/audio_analysis.py
@@ -45,6 +45,10 @@ def process(recording, jwtKey, conf):
     Returns:
         The API response.
     """
+
+    # this used to work by default then  just stopped, so will explicitly add it
+    mimetypes.add_type("audio/mp4", ".m4a")
+
     logger = logs.worker_logger("audio.analysis", recording["id"])
 
     api = API(conf.api_url, conf.user, conf.password, logger)
