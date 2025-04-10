08d9701f2fd26209db8d241c203cad23d79dc09a
gferraro <g.ferraro22@gmail.com>
2025-04-10 17:02:52 +1200

    added audio anlaysis track parsing

--- processing/audio_analysis.py
+++ processing/audio_analysis.py
@@ -35,7 +35,7 @@ MAX_FRQUENCY = 48000 / 2
 def track_analyse(recording, jwtKey, conf):
     """Reprocess the audio file.
 
-    Downloads the file, runs the AI model on tracks that don't have AI tags
+    Downloads the file, runs the AI model on tracks that have been made by users and dont yet have an AI tag
 
     Args:
         recording: The recording to process.
@@ -77,41 +77,35 @@ def track_analyse(recording, jwtKey, conf):
         ]
         recording["Tracks"] = track_info
         filename = input_filename.with_suffix(".txt")
+        if "location" in recording:
+            location = recording["location"]
+            if (
+                "lat" not in location
+                and "lng" not in location
+                and "coordinates" in location
+            ):
+                coords = location["coordinates"]
+                location["lng"] = coords[0]
+                location["lat"] = coords[1]
         with filename.open("w") as f:
             json.dump(recording, f)
 
         metadata = analyse(input_filename, conf, analyse_tracks=True)
-        analysis = metadata.get("analysis_result")
-        if analysis["species_identify"]:
-            species_identify = analysis.pop("species_identify")
-            for analysis_result in species_identify:
-                model_name = analysis_result.get("model", "Unnamed")
-                predictions = analysis_result["predictions"]
-                for i, prediction in enumerate(predictions):
-                    species = prediction["species"]
-                    confidences = prediction["likelihood"]
-                    del prediction["species"]
-                    raw_tag = None
-                    if len(confidences) == 0 and "raw_tag" in prediction:
-                        raw_tag = prediction["raw_tag"]
-                        species = [UNIDENTIFIED]
-                        confidences = [prediction["raw_confidence"]]
+        analysis = AudioResult.load(metadata, metadata.get("duration"))
+        algorithm_meta = {"algorithm": "sliding_window"}
+        if analysis.species_identify_version is not None:
+            algorithm_meta["version"] = analysis.species_identify_version
+        algorithm_id = api.get_algorithm_id(algorithm_meta)
+        for track in analysis.tracks:
+            for i, prediction in enumerate(track.predictions):
+                data = {"algorithm": algorithm_id}
 
-                    for confidence, s in zip(confidences, species):
-                        pred = Prediction(confidence=confidence, tag=s)
-                        data = {"name": "Master"}
-                        if raw_tag is not None:
-                            data["raw_tag"] = raw_tag
-
-                        if i == 0:
-                            # just add master tag for first prediction
-                            api.add_track_tag(
-                                recording, analysis_result["track_id"], pred, data
-                            )
-                        data["name"] = prediction["model"]
-                        api.add_track_tag(
-                            recording, analysis_result["track_id"], pred, data
-                        )
+                if i == 0:
+                    data["name"] = "Master"
+                    # just add master tag for first prediction
+                    api.add_track_tag(recording, track.id, prediction, data)
+                data["name"] = prediction.model_name
+                api.add_track_tag(recording, track.id, prediction, data)
 
     api.report_done(recording, metadata=new_metadata)
     logger.info("Completed classifying for file: %s", recording["id"])
@@ -156,80 +150,40 @@ def process(recording, jwtKey, conf):
         logger.debug("downloading recording to %s", input_filename)
         api.download_file(jwtKey, str(input_filename))
         metadata = analyse(input_filename, conf)
-        new_metadata = {}
-        analysis = metadata.get("analysis_result")
-        duration = analysis.get("duration")
+        new_metadata = {"additionalMetadata": {}}
+        duration = recording.get("duration")
         if duration is not None:
             new_metadata["duration"] = duration
         else:
-            duration = metadata.get("duration")
-        if analysis["species_identify"]:
-            species_identify = analysis.pop("species_identify")
-            for analysis_result in species_identify:
-                model_name = analysis_result.get("model", "Unnamed")
-                start_s = analysis_result["begin_s"]
-                end_s = analysis_result["end_s"]
-                x = start_s / duration
-                width = end_s / duration - x
-                y = 0
-                height = 1
-                position = {}
-                track = {}
-                if "freq_end" in analysis_result:
-                    y = analysis_result["freq_start"] / MAX_FRQUENCY
-                    height = (
-                        analysis_result["freq_end"] - analysis_result["freq_start"]
-                    ) / MAX_FRQUENCY
-                    track["minFreq"] = analysis_result["freq_start"]
-                    track["maxFreq"] = analysis_result["freq_end"]
-                track["scale"] = "linear"
-                # convert to 2 decimal places
-                x = round(x, 2)
-                width = round(width, 2)
-                position = {
-                    "x": x,
-                    "y": y,
-                    "width": width,
-                    "height": height,
-                }
-                track.update(
-                    {
-                        "start_s": start_s,
-                        "end_s": end_s,
-                        "positions": [position],
-                    }
-                )
-                algorithm_id = api.get_algorithm_id({"algorithm": "sliding_window"})
-                id = api.add_track(recording, track, algorithm_id)
-                predictions = analysis_result["predictions"]
-                for i, prediction in enumerate(predictions):
-                    species = prediction["species"]
-                    confidences = prediction["likelihood"]
-                    del prediction["species"]
-                    raw_tag = None
-                    if len(confidences) == 0 and "raw_tag" in prediction:
-                        raw_tag = prediction["raw_tag"]
-                        species = [UNIDENTIFIED]
-                        confidences = [prediction["raw_confidence"]]
+            duration = metadata.get("analysis_result", {}).get("duration")
+        analysis = AudioResult.load(metadata, duration)
+        algorithm_meta = {"algorithm": "sliding_window"}
+        if analysis.species_identify_version is not None:
+            algorithm_meta["version"] = analysis.species_identify_version
+        algorithm_id = api.get_algorithm_id(algorithm_meta)
 
-                    for confidence, s in zip(confidences, species):
-                        pred = Prediction(confidence=confidence, tag=s)
-                        data = {"name": "Master"}
-                        if raw_tag is not None:
-                            data["raw_tag"] = raw_tag
+        for track in analysis.tracks:
+            track.id = api.add_track(recording, track, algorithm_id)
+            for i, prediction in enumerate(track.predictions):
+                data = {"algorithm": algorithm_id}
+                if i == 0:
+                    data["name"] = "Master"
+                    # just add master tag for first prediction
+                    api.add_track_tag(recording, track.id, prediction, data)
+                data["name"] = prediction.model_name
+                api.add_track_tag(recording, track.id, prediction, data)
 
-                        if i == 0:
-                            # just add master tag for first prediction
-                            api.add_track_tag(recording, id, pred, data)
-                        data["name"] = prediction["model"]
-                        api.add_track_tag(recording, id, pred, data)
-
-        if analysis["cacophony_index"]:
-            cacophony_index = analysis.pop("cacophony_index")
-            new_metadata["cacophonyIndex"] = cacophony_index
-            logger.info("cacophony_index: %s", cacophony_index)
-
-        new_metadata["additionalMetadata"] = analysis
+        if analysis.cacophony_index is not None:
+            new_metadata["cacophonyIndex"] = analysis.cacophony_index
+            new_metadata["additionalMetadata"][
+                "cacophony_index_version"
+            ] = analysis.cacophony_index_version
+        if analysis.chirp_index is not None:
+            new_metadata["additionalMetadata"]["chirpIndex"] = analysis.chirp_index
+        if analysis.region_code is not None:
+            new_metadata["additionalMetadata"]["regionCode"] = analysis.region_code
+        # is there anyhting missing...
+        # new_metadata["additionalMetadata"] = analysis
 
     api.report_done(recording, metadata=new_metadata)
     logger.info("Completed processing for file: %s", recording["id"])
@@ -242,12 +196,125 @@ def analyse(filename, conf, analyse_tracks=False):
         tag=conf.audio_analysis_tag,
         analyse_tracks=analyse_tracks,
     )
-    # Should change this to read from a file
     with HandleCalledProcessError():
-        subprocess.run(
-            command, shell=True, stderr=subprocess.PIPE, timeout=conf.subprocess_timeout
+        proc = subprocess.run(
+            command,
+            shell=True,
+            stderr=subprocess.PIPE,
+            timeout=conf.subprocess_timeout,
+            check=True,
         )
     meta_f = Path(filename).with_suffix(".txt")
     with meta_f.open("r") as f:
         classify_info = json.load(f)
     return classify_info
+
+
+import attr
+
+
+@attr.s
+class AudioResult:
+    tracks = attr.ib()
+    duration = attr.ib()
+    cacophony_index = attr.ib()
+    cacophony_index_version = attr.ib()
+    chirp_index = attr.ib()
+    region_code = attr.ib()
+    species_identify_version = attr.ib()
+
+    @classmethod
+    def load(cls, result, duration):
+        tracks = []
+        analysis = result.get("analysis_result", {})
+        for track in analysis.get("species_identify", []):
+            tracks.append(AudioTrack.load(track, duration))
+
+        return cls(
+            tracks=tracks,
+            duration=duration,
+            cacophony_index=result.get("cacophony_index"),
+            chirp_index=result.get("chirps"),
+            cacophony_index_version=result.get("cacophony_index_version"),
+            region_code=result.get("region_code"),
+            species_identify_version=result.get("species_identify_version"),
+        )
+
+
+@attr.s
+class AudioTrack:
+    id = attr.ib()
+    predictions = attr.ib()
+    min_freq = attr.ib()
+    max_freq = attr.ib()
+    start_s = attr.ib()
+    end_s = attr.ib()
+    scale = attr.ib()
+    master_tag = attr.ib(default=None)
+    positions = attr.ib(default=None)
+
+    @classmethod
+    def load(cls, raw_track, duration):
+        preds = []
+        for prediction in raw_track.get("predictions"):
+            species = prediction["species"]
+            confidences = prediction["likelihood"]
+            del prediction["species"]
+            raw_tag = None
+            if len(confidences) == 0 and "raw_tag" in prediction:
+                raw_tag = prediction["raw_tag"]
+                species = [UNIDENTIFIED]
+                confidences = [prediction["raw_confidence"]]
+
+            for confidence, s in zip(confidences, species):
+                pred = Prediction(
+                    confidence=confidence,
+                    tag=s,
+                    label=raw_tag,
+                    model_name=prediction["model"],
+                )
+                preds.append(pred)
+
+            track = cls(
+                id=raw_track.get("track_id"),
+                predictions=preds,
+                start_s=raw_track.get("begin_s"),
+                end_s=raw_track.get("end_s"),
+                min_freq=raw_track.get("freq_start"),
+                max_freq=raw_track.get("freq_end"),
+                scale="linear",
+            )
+
+            # dont think we need this anymore ask JON
+            x = track.start_s / duration
+            width = track.end_s / duration - x
+            y = 0
+            height = 1
+            position = {}
+            if track.max_freq is not None:
+                y = track.min_freq / MAX_FRQUENCY
+                height = (track.max_freq - track.min_freq) / MAX_FRQUENCY
+
+            # convert to 2 decimal places
+            x = round(x, 2)
+            width = round(width, 2)
+            position = {
+                "x": x,
+                "y": y,
+                "width": width,
+                "height": height,
+            }
+            track.positions = [position]
+            return track
+
+    def post_data(self):
+        data = {
+            "positions": self.positions,
+            "start_s": self.start_s,
+            "end_s": self.end_s,
+            "minFreq": self.min_freq,
+            "maxFreq": self.max_freq,
+        }
+        if self.id is not None:
+            data["id"] = self.id
+        return data
